97f6580a33ba4af709e0001d4d0101e58e392670
kjs4395 <kjs4395@gmail.com>
2018-02-06 01:18:36 +0900

    답변 api

---
+++ src/main/java/codesquad/DeleteException.java
@@ -0,0 +1,8 @@
+public class DeleteException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public DeleteException(String message) {
+        super(message);
+    }
+}
